b2ba57a1d3673a4886674727d9a8a039ec7a14f9
xziy <cziy@ya.ru>
2017-04-15 16:54:31 +0400

    Update routes.rb
    mutch fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
-  match '/1c_exchange.php' => 'exchange1c#main'
-  match '/test' => 'exchange1c#test'
+  match '/1c_exchange.php' => 'exchange1c#main', via: [:get, :post]
+  match '/test' => 'exchange1c#test', via: [:get, :post]
 
 end
