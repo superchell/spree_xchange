81d140004247c304981f78a5a4b570d0e24e5451
Oleg Mozolev <superchell@users.noreply.github.com>
2017-11-10 11:22:24 +0200

    Rails 5 migration support

--- db/migrate/20120628112601_add1c_id.rb
+++ db/migrate/20120628112601_add1c_id.rb
@@ -1,30 +1,7 @@
-class Add1cId < ActiveRecord::Migration
+class Add1cId < ActiveRecord::Migration[4.2]
   def change
-    # add_column :spree_products, :xchange_id, 'binary(16)'
-    # add_column :spree_variants, :xchange_id, 'binary(16)'
-    # add_column :spree_taxons, :xchange_id, 'binary(16)'
     add_column :spree_products, :xchange_id, :bytea
     add_column :spree_variants, :xchange_id, :bytea
     add_column :spree_taxons, :xchange_id, :bytea
   end
-#  def up
-#    #, ADD UNIQUE(xchange_id)
-#    execute <<-SQL
-#    ALTER TABLE spree_products ADD xchange_id binary(16);
-#    ALTER TABLE spree_variants ADD xchange_id binary(16);
-#    ALTER TABLE spree_taxons ADD xchange_id binary(16);
-#SQL
-#  end
-#  def down
-#    execute <<-SQL
-#    ALTER TABLE spree_products DROP xchange_id;
-#    ALTER TABLE spree_variants DROP xchange_id;
-#    ALTER TABLE spree_taxons DROP xchange_id;
-#    SQL
-#  end
-#
-#  #def change
-#  #  add_column :spree_products, :xchange_id, :binary, :null=>false, :limit=>16
-#  #end
-
 end
